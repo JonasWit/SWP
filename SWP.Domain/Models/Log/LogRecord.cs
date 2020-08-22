d1b1505a7d43f775b1cd6739a86a0957a9fb1151
Viers <jonaszwitek@gmail.com>
2020-08-22 18:32:03 +0200

    log filder added

---
+++ SWP.Domain/Models/Log/LogRecord.cs
@@ -0,0 +1,23 @@
+﻿using SWP.Domain.Models.General;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SWP.Domain.Models.Log
+{
+    public class LogRecord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Message { get; set; }
+
+        public int UserDataId { get; set; }
+        public UserData UserData { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}

51f0c37f1acb9b6ff9164c5e19577151e120e4e1
Viers <jonaszwitek@gmail.com>
2020-09-29 14:46:14 +0200

    productivity added

--- SWP.UI/Components/LegalSwpBlazorComponents/App/MyAppPage.cs
+++ SWP.UI/Components/LegalSwpBlazorComponents/App/MyAppPage.cs
@@ -61,12 +61,14 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.App
                 RefreshClientCases();
                 RefreshSpecificFinanceData();
                 RefreshSpecificTimeData();
+                RefreshSpecificProductivityData();
             }
             else
             {
                 RefreshClientCases();
                 RefreshAllFinanceData();
                 RefreshAllTimeData();
+                RefreshAllProductivityData();
             }
         }
 
@@ -188,6 +190,65 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.App
             }
 
             TimeData.Add(dataSet);
+        }
+
+        private void RefreshSpecificProductivityData()
+        {
+            ProductivityData.Clear();
+
+            var dataSet = new ClientData
+            {
+                Name = App.ActiveClient.Name
+            };
+
+            var timeRecords = GetTimeRecords.Get(App.ActiveClient.Id);
+            var cashMovements = GetCashMovements.Get(App.ActiveClient.Id);
+
+            for (int i = 0; i < 13; i++)
+            {
+                var date = new DateTime(DateTime.Now.AddMonths(-i).Year, DateTime.Now.AddMonths(-i).Month, DateTime.DaysInMonth(DateTime.Now.AddMonths(-i).Year, DateTime.Now.AddMonths(-i).Month));
+                var records = timeRecords.Where(x => x.EventDate <= date).ToList();
+
+                dataSet.DataByDate.Add(new DateDataItem
+                {
+                    Date = date,
+                    Time = new TimeSpan(timeRecords.Where(x => x.EventDate <= date).Sum(x => x.Hours), timeRecords.Where(x => x.EventDate <= date).Sum(x => x.Minutes), 0),
+                    Number = Math.Round(cashMovements.Where(x => x.EventDate <= date).Sum(x => x.Amount), 2),
+                });
+            }
+
+            ProductivityData.Add(dataSet);
+        }
+
+        private void RefreshAllProductivityData()
+        {
+            ProductivityData.Clear();
+
+            foreach (var client in App.Clients)
+            {
+                var dataSet = new ClientData
+                {
+                    Name = client.Name
+                };
+
+                var timeRecords = GetTimeRecords.Get(client.Id);
+                var cashMovements = GetCashMovements.Get(client.Id);
+
+                for (int i = 0; i < 13; i++)
+                {
+                    var date = new DateTime(DateTime.Now.AddMonths(-i).Year, DateTime.Now.AddMonths(-i).Month, DateTime.DaysInMonth(DateTime.Now.AddMonths(-i).Year, DateTime.Now.AddMonths(-i).Month));
+                    var records = timeRecords.Where(x => x.EventDate <= date).ToList();
+
+                    dataSet.DataByDate.Add(new DateDataItem
+                    {
+                        Date = date,
+                        Time = new TimeSpan(timeRecords.Where(x => x.EventDate <= date).Sum(x => x.Hours), timeRecords.Where(x => x.EventDate <= date).Sum(x => x.Minutes), 0),
+                        Number = Math.Round(cashMovements.Where(x => x.EventDate <= date).Sum(x => x.Amount), 2),
+                    });
+                }
+
+                ProductivityData.Add(dataSet);
+            }
         }
 
         public void Dispose()
--- SWP.UI/Components/LegalSwpBlazorComponents/ViewModels/Data/Statistics/DateDataItem.cs
+++ SWP.UI/Components/LegalSwpBlazorComponents/ViewModels/Data/Statistics/DateDataItem.cs
@@ -8,8 +8,10 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.ViewModels.Data.Statistics
     public class DateDataItem
     {
         public double TimeNumber => Time != null ? (((double)Time.Days * 24) + (double)Time.Hours + (((double)Time.Minutes / 100))) : 0;
+        public int TotalHours => Time != null ? (Time.Days * 24) + Time.Hours : 0;
         public TimeSpan Time { get; set; }
         public DateTime Date { get; set; }
         public double Number { get; set; } = 0;
+        public double ProductivityRatio => TotalHours != 0 ? Math.Round((Number / TotalHours), 2) : 0;
     }
 }

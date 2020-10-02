938e6eba059dc71490c8f0d118d0bea64ab7ff97
Viers <jonaszwitek@gmail.com>
2020-10-02 23:21:49 +0200

    wip

---
+++ SWP.Application/Log/DeleteLogRecord.cs
@@ -0,0 +1,18 @@
+﻿using SWP.Domain.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP.Application.Log
+{
+    [TransientService]
+    public class DeleteLogRecord
+    {
+        private readonly ILogManager logManager;
+        public DeleteLogRecord(ILogManager logManager) => this.logManager = logManager;
+
+        public Task<int> Delete(int id) => logManager.DeleteLogRecord(id);
+
+    }
+}
---
+++ SWP.Application/Log/GetLogRecord.cs
@@ -0,0 +1,17 @@
+﻿using SWP.Domain.Infrastructure;
+using SWP.Domain.Models.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWP.Application.Log
+{
+    [TransientService]
+    public class GetLogRecord
+    {
+        private readonly ILogManager logManager;
+        public GetLogRecord(ILogManager logManager) => this.logManager = logManager;
+
+        public LogRecord Get(int id) => logManager.GetLogRecord(id);
+    }
+}

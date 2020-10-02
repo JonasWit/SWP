394092201b6be2c5daa22274a5ecfbdc5e3bb57c
Viers <jonaszwitek@gmail.com>
2020-10-02 10:59:20 +0200

    wip

--- SWP.UI/BlazorApp/BlazorPageCore.cs
+++ SWP.UI/BlazorApp/BlazorPageCore.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using SWP.Application.Log;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SWP.UI.BlazorApp
 {
     public abstract class BlazorPageCore
     {
         protected readonly IServiceProvider serviceProvider;
+
+        private CreateLogEntry CreateLogEntry => serviceProvider.GetService<CreateLogEntry>();
 
         public BlazorPageCore(IServiceProvider serviceProvider) => this.serviceProvider = serviceProvider;
 

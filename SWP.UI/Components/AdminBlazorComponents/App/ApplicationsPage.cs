4a3c77dbff694e5a344e2c49d8f3fe220e3ef31b
Viers <jonaszwitek@gmail.com>
2020-10-03 14:16:44 +0200

    log updates

--- SWP.UI/Components/AdminBlazorComponents/App/ApplicationsPage.cs
+++ SWP.UI/Components/AdminBlazorComponents/App/ApplicationsPage.cs
@@ -1,4 +1,6 @@
-﻿using SWP.UI.BlazorApp;
+﻿using SWP.Domain.Models.Log;
+using SWP.UI.BlazorApp;
+using SWP.UI.Components.AdminBlazorComponents.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,22 +12,34 @@ namespace SWP.UI.Components.AdminBlazorComponents.App
     public class ApplicationsPage : BlazorPageBase
     {
         public AdminBlazorApp App { get; set; }
+        public LogRecordViewModel SelectedLogRecord { get; set; }
+        public List<LogRecordViewModel> LogRecords { get; set; } = new List<LogRecordViewModel>();
 
         public ApplicationsPage(IServiceProvider serviceProvider) : base(serviceProvider) { }
 
         public override Task Initialize(BlazorAppBase app)
         {
             App = app as AdminBlazorApp;
+            RefreshLog();
             return Task.CompletedTask;
         }
 
-        public void RefreshLog()
-        { 
-        
-        
-        
-        
-        }
+        public void RefreshLog() => LogRecords = new List<LogRecordViewModel>(GetLogs().Select(x => (LogRecordViewModel)x));
 
+        public void RowSelected(object args) => SelectedLogRecord = (LogRecordViewModel)args;
+
+        public async Task DeleteLogRecord(LogRecordViewModel logRecord)
+        {
+            try
+            {
+
+
+
+            }
+            catch (Exception ex)
+            {
+                App.ErrorPage.DisplayMessage(ex);
+            }
+        }
     }
 }
---
+++ SWP.UI/Components/AdminBlazorComponents/ViewModels/LogRecordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SWP.Domain.Models.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP.UI.Components.AdminBlazorComponents.ViewModels
+{
+    public class LogRecordViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+        public DateTime Created { get; set; }
+
+        public static implicit operator LogRecordViewModel(LogRecord input) =>
+            new LogRecord
+            {
+                Id = input.Id,
+                UserId = input.UserId,
+                Message = input.Message,
+                StackTrace = input.StackTrace,
+                Created = input.Created
+            };
+    }
+}

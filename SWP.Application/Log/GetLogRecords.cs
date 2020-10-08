236eb6fe4b61e97d7a7262788e3c870f6873e46d
Viers <jonaszwitek@gmail.com>
2020-10-08 09:03:05 +0200

    log added to source

---
+++ SWP.Application/Log/GetLogRecords.cs
@@ -0,0 +1,26 @@
+﻿using SWP.Domain.Infrastructure;
+using SWP.Domain.Models.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWP.Application.Log
+{
+    [TransientService]
+    public class GetLogRecords
+    {
+        private readonly ILogManager logManager;
+        public GetLogRecords(ILogManager logManager) => this.logManager = logManager;
+
+        public List<LogRecord> GetRecords() => logManager.GetGetLogRecords();
+
+        public List<LogRecord> GetRecordsWitohutStacks() => 
+            logManager.GetLogRecordsWithSpecificData(x => new LogRecord 
+            { 
+                Id = x.Id, 
+                Created = x.Created,
+                Message = x.Message,
+                UserId = x.UserId 
+            });
+    }
+}
---
+++ SWP.Domain/Models/Log/LogRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SWP.Domain.Models.Log
+{
+    public class LogRecord
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+    }
+}

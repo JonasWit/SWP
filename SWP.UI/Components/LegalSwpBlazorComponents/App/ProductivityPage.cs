69201efe29efe105aae6234b4f894dce5a036327
Viers <jonaszwitek@gmail.com>
2020-10-12 15:36:40 +0200

    font drop

--- SWP.UI/Components/LegalSwpBlazorComponents/App/ProductivityPage.cs
+++ SWP.UI/Components/LegalSwpBlazorComponents/App/ProductivityPage.cs
@@ -27,10 +27,21 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.App
 
         public ProductivityPage(IServiceProvider serviceProvider) : base(serviceProvider) { }
 
+        public FontFilterRecord SelectedFont { get; set; }
+        public List<FontFilterRecord> FontsFilterData { get; set; } = new List<FontFilterRecord>();
+
+        public class FontFilterRecord
+        {
+            public int Id { get; set; }
+            public string DisplayText { get; set; }
+            public string FontName { get; set; }
+        }
+
         public override Task Initialize(BlazorAppBase app)
         {
             App = app as LegalBlazorApp;
             GetDataForMonthFilter();
+            GetDataForFontFilter();
             return Task.CompletedTask;
         }
 
@@ -160,6 +171,18 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.App
             }
         }
 
+        public void GetDataForFontFilter()
+        {
+            FontsFilterData.Clear();
+
+            FontsFilterData.Add(new FontFilterRecord
+            {
+                Id = 1,
+                FontName = "Anonymous_Pro",
+                DisplayText = "Old-Computer"
+            });
+        }
+
         public void SelectedMonthChange(object value)
         {
             var input = (int?)value;
@@ -173,11 +196,33 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.App
             }
         }
 
+        public void SelectedFontChange(object value)
+        {
+            var input = (int?)value;
+            if (input != null)
+            {
+                SelectedFont = FontsFilterData.FirstOrDefault(x => x.Id == input);
+            }
+            else
+            {
+                SelectedFont = null;
+            }
+        }
+
         public async Task GenerateTimesheetReport(LegalTimeSheetReport.ReportData reportData)
         {
             try
             {
                 var productivityRecords = new List<TimeRecordViewModel>();
+
+                if (SelectedFont != null)
+                {
+                    reportData.FontName = SelectedFont.FontName;
+                }
+                else
+                {
+                    reportData.FontName = "Anonymous_Pro";
+                }
 
                 if (reportData.UseSelectedMonth)
                 {
@@ -210,7 +255,6 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents.App
                     return;
                 }
 
-                reportData.FontName = "Anonymous_Pro";
                 reportData.ClientName = App.ActiveClient.Name;
                 reportData.Records = productivityRecords;
                 reportData.ReportName = $"{reportData.ClientName}_{DateTime.Now:yyyy-MM-dd-hh-mm-ss}";

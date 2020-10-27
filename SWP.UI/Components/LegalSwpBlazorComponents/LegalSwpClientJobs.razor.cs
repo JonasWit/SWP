74cf2e352747a4b1ee8ab44d36e94fef576f54e4
Viers <jonaszwitek@gmail.com>
2020-10-27 23:01:55 +0100

    Clients tasks

--- SWP.UI/Components/LegalSwpBlazorComponents/LegalSwpClientJobs.razor.cs
+++ SWP.UI/Components/LegalSwpBlazorComponents/LegalSwpClientJobs.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Radzen;
 using SWP.UI.Components.LegalSwpBlazorComponents.App;
 using SWP.UI.Components.LegalSwpBlazorComponents.ViewModels.Data;
 using System;
@@ -15,6 +16,9 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents
         [Inject]
         public GeneralViewModel Gvm { get; set; }
 
+        [Inject]
+        public TooltipService TooltipService { get; set; }
+
         public string ArchvizedJobsFilterValue;
 
         public bool showFormVisible = false;
@@ -23,6 +27,8 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents
         public bool addClientformVisible = false;
         public void ShowHideClientFormI() => addClientformVisible = !addClientformVisible;
 
+        public bool showSecondSection = false;
+        public void ShowHideSecondSection() => showSecondSection = !showSecondSection;
 
     }
 }

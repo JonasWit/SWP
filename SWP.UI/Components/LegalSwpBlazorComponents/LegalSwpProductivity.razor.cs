19daf565a2b2e169d5e76f3214440409a9723981
Viers <jonaszwitek@gmail.com>
2020-11-01 00:50:39 +0100

    Update

--- SWP.UI/Components/LegalSwpBlazorComponents/LegalSwpProductivity.razor.cs
+++ SWP.UI/Components/LegalSwpBlazorComponents/LegalSwpProductivity.razor.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Radzen;
 using SWP.UI.Components.LegalSwpBlazorComponents.App;
+using SWP.UI.Components.LegalSwpBlazorComponents.ViewModels.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,27 @@ namespace SWP.UI.Components.LegalSwpBlazorComponents
     {
         [Inject]
         public LegalBlazorApp App { get; set; }
+        [Inject]
+        public GeneralViewModel Gvm { get; set; }
+        [Inject]
+        public TooltipService TooltipService { get; set; }
+
+        public bool showFirstSection = false;
+        public void ShowHideFirstSection() => showFirstSection = !showFirstSection;
+
+        public bool showSecondSection = false;
+        public void ShowHideSecondSection() => showSecondSection = !showSecondSection;
+        
+        public bool showThirdSection = false;
+        public void ShowHideThirdSection() => showThirdSection = !showThirdSection;
+
+        public bool infoBoxVisibleI = false;
+        public void ShowHideInfoBoxI() => infoBoxVisibleI = !infoBoxVisibleI;
+
+        public bool infoBoxVisibleII = false;
+        public void ShowHideInfoBoxII() => infoBoxVisibleII = !infoBoxVisibleII;
+
+        public bool infoBoxVisibleIII = false;
+        public void ShowHideInfoBoxIII() => infoBoxVisibleIII = !infoBoxVisibleIII;
     }
 }

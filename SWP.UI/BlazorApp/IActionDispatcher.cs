06684165dcd9ce6121e408dbfb1ff5c9294a9664
Viers <jonaszwitek@gmail.com>
2020-11-06 13:51:19 +0100

    admin flux prep

--- SWP.UI/BlazorApp/IActionDispatcher.cs
+++ SWP.UI/BlazorApp/IActionDispatcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SWP.UI.BlazorApp.LegalApp
+namespace SWP.UI.BlazorApp
 {
     public interface IActionDispatcher
     {

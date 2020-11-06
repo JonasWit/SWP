4d95e483f34e3e33a148a24a9566794317f698c9
Viers <jonaszwitek@gmail.com>
2020-11-06 13:55:14 +0100

    admin flux wip

---
+++ SWP.UI/BlazorApp/AdminApp/Stores/ApplicationStore/Actions/InitializeAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP.UI.BlazorApp.AdminApp.Stores.ApplicationStore.Actions
+{
+    public class InitializeAction : IAction
+    {
+        public const string Initialize = "Initialize";
+        public string Name => Initialize;
+        public string UserId { get; set; }
+    }
+}
---
+++ SWP.UI/BlazorApp/AdminApp/Stores/ApplicationStore/ApplicationStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP.UI.BlazorApp.AdminApp.Stores.ApplicationStore
+{
+    public class ApplicationState
+    {
+        public bool Loading { get; set; } = false;
+
+    }
+
+    public class ApplicationStore
+    {
+    }
+}

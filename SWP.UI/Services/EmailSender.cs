7035c176de701c75e9a683b5c51c7ef33f659fe5
Viers <jonaszwitek@gmail.com>
2020-11-06 14:02:47 +0100

    nugets updated

--- SWP.UI/Services/EmailSender.cs
+++ SWP.UI/Services/EmailSender.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using NETCore.MailKit.Core;
-using SendGrid;
-using SendGrid.Helpers.Mail;
-using System;
 using System.Threading.Tasks;
 
 namespace SWP.UI.Services

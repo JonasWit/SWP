91d0d312ce58b36df4f336b80e61f7ee44350f09
Viers <jonaszwitek@gmail.com>
2020-11-30 14:23:49 +0100

    client details actions done

---
+++ SWP.UI/BlazorApp/LegalApp/Stores/ClientDetails/Actions/EditContactRowAction.cs
@@ -0,0 +1,16 @@
+﻿using SWP.UI.Components.LegalSwpBlazorComponents.ViewModels.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP.UI.BlazorApp.LegalApp.Stores.ClientDetails.Actions
+{
+    public class EditContactRowAction : IAction
+    {
+        public const string EditContactRow = "EDIT_CONTACT_ROW";
+        public string Name => EditContactRow;
+
+        public ContactPersonViewModel Arg { get; set; }
+    }
+}

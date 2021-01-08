270f742b025844406cc19be3c76a9f1c18dde398
Viers <54804051+ViersJW@users.noreply.github.com>
2021-01-08 13:35:31 +0100

    news updates

---
+++ SWP.UI/Components/PortalBlazorComponents/NewsAdminPage.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using SWP.UI.BlazorApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWP.UI.Components.PortalBlazorComponents
+{
+    public partial class NewsAdminPage : IDisposable
+    {
+        [Inject]
+        public IActionDispatcher ActionDispatcher { get; set; }
+
+        public void Dispose()
+        {
+         
+        }
+
+        private void UpdateView() => StateHasChanged();
+
+        protected override void OnInitialized()
+        {
+        
+ 
+        }
+
+        #region Actions
+
+
+
+
+
+
+
+
+
+
+        #endregion
+
+
+    }
+}

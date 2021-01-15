4ca3529268ad6d9082face529bdf0dabeaccd5f4
Viers <54804051+ViersJW@users.noreply.github.com>
2021-01-15 23:27:33 +0100

    New page updates

--- SWP.UI/Pages/Marketing/WebPages/Index.cshtml.cs
+++ SWP.UI/Pages/Marketing/WebPages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SWP.UI.Pages.Marketing.IndividualSolutions
+namespace SWP.UI.Pages.Marketing.WebPages
 {
     public class IndexModel : PageModel
     {
